66f24fe2cb4ceff81d7074311939722288634821
administrador1 <administrador1@LS_11>
2016-08-09 17:15:00 -0500

    Segundo Commit

---
+++ src/tallerjava2016/PruebaSimpleDeGit.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallerjava2016;
+
+/**
+ *
+ * @author administrador1
+ */
+public class PruebaSimpleDeGit {
+    public static void main(String[] args) {
+        System.out.println("bienvenidos al curso");
+    }
+}
